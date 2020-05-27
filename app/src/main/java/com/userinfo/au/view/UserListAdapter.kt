73fa1b5afde146989041ad7232eb5b1b49ac6d05
Asha Ramanjanaiah <asha.ramanjanaiah@gmail.com>
2020-05-28 02:38:51 +1000

    - Added databinding to bind datasource to the view in layout.

--- app/src/main/java/com/userinfo/au/view/UserListAdapter.kt
+++ app/src/main/java/com/userinfo/au/view/UserListAdapter.kt
@@ -3,8 +3,10 @@ package com.userinfo.au.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.userinfo.au.R
+import com.userinfo.au.databinding.UserListItemBinding
 import com.userinfo.au.model.User
 import kotlinx.android.synthetic.main.user_list_item.view.*
 
@@ -17,7 +19,10 @@ class UserListAdapter(private val usersList: ArrayList<User>): RecyclerView.Adap
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.user_list_item, parent, false)
+        val view = DataBindingUtil.inflate<UserListItemBinding>(
+            inflater,
+            R.layout.user_list_item, parent, false
+        )
         return UserListViewHolder(view)
     }
 
@@ -26,11 +31,8 @@ class UserListAdapter(private val usersList: ArrayList<User>): RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
-        holder.view.tv_user_id.text = usersList[0].id.toString()
-        holder.view.tv_name.text = usersList[0].name
-        holder.view.tv_email.text = usersList[0].email
-        holder.view.tv_phone.text = usersList[0].phone
+        holder.view.user = usersList[position]
     }
 
-    class UserListViewHolder(var view: View): RecyclerView.ViewHolder(view)
+    class UserListViewHolder(var view: UserListItemBinding): RecyclerView.ViewHolder(view.root)
 }
